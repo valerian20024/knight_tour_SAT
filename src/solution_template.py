45e6612a06fbd23d46270d2aaac6cf51c48fdc9b
Wislez Valérian <wislez.valerian@gmail.com>
2026-08-19 12:21:23 +0200

    Reworkeds comments in solution_template

--- src/solution_template.py
+++ src/solution_template.py
@@ -3,37 +3,33 @@ from symmetry import count_solutions_up_to_symmetry
 from plot import rainbow_plot
 from helpers import leave_one_out_subsets
 
-"""
-The first question implementation. Solves the Knight's Tour problem.
-
-@param M: The number of rows in the chessboard.
-@param N: The number of columns in the chessboard.
-@param i0: The start row (0-indexed)
-@param j0: The start column (0-indexed)
-
-@return solution: A list of lists, such that solution[i][j] is the number of
-moves required to arrive in cell (i, j). If no solution is found, every cell
-value is set to -1.
-@return solver: The instance of the solver used.
-@return variables: The list of variables used.
-
-"""
 def question1(M, N, i0, j0):
-    T = M * N
+    """Solve the Knight's Tour problem.
     
+    @param M:   Number of chessboard rows.
+    @param N:   Number of chessboard cols.
+    @param i0:  Knight's initial row (0-indexed).
+    @param j0:  Knight's initial col (0-indexed).
+
+    @return solution: 
+        solution[i][j] is the number of moves required to arrive in cell 
+        (i, j). If no solution is found, every cell value is set to -1.
+    @return solver: 
+        The instance of the solver used.
+    @return variables: 
+        The variables used.
+    """
+    T = M * N
     solver, variables = build_knight_tour(M, N, i0, j0)
     solution, _ = extract_solution(solver, M, N, T, variables)
     
     return solution, solver, list(variables.values())
 
-"""
-The third question implementation.
 
-@return nb_sol: The number of solutions for a given instance of the problem.
-"""
 def question3():
-    M = 5
-    N = 5
+    """Computes the number of solutions for a given instance of the problem."""
+    M = 3
+    N = 4
     T = M * N
 
     nb_sol = 0
@@ -41,23 +37,19 @@ def question3():
         for j0 in range(N):
             solver, variables = build_knight_tour(M, N, i0, j0)
             solutions, _ = extract_all_solutions(solver, M, N, T, variables)
-            """for sol in solutions:
+            """
+            for sol in solutions:
                 print("sol")
                 for row in sol:
-                    print(f"{row}")"""
+                    print(f"{row}")
+            """
 
             nb_sol += len(solutions)
-    
     return nb_sol
 
-
-"""
-The fourth question implementation. Counts the number of solutions up to symmetry. 
-Two solutions are identical if one can be obtained by applying a symmetry to the other.
-
-@return nb_sol: The number of solutions up to symmetry of the problem.
-"""
 def question4():
+    """Counts the number of solutions up to symmetry."""
+
     M = 3
     N = 4
     T = M * N
@@ -78,20 +70,17 @@ def question4():
             
     return nb_sol
 
-"""
-The fifth question implementation. Computes the constraints that guarantee
-there is a unique solution. All constraints are necessary for uniqueness
-of the solution. They enforce the knight to visit a specific cell at a specific timestep.
 
-@param M: The number of rows in the chessboard.
-@param N: The number of columns in the chessboard.
-@param i0: The start row (0-indexed)
-@param j0: The start column (0-indexed)
-
-@return constraints: Constraints are of the form [(t1, i1, j1), (t2, i2, j2), ...]. If the function is called with parameters such that
-no solution exists, it returns an empty list.
-"""
 def question5(M, N, i0, j0):
+    """Computes the constraints guaranteeing a unique solution. 
+
+    All constraints are necessary for uniqueness of the solution. 
+    They enforce the knight to visit a specific cell at a specific timestep.
+
+    @return constraints: Constraints are of the form [(t1, i1, j1), (t2, i2, j2), ...]. If the function is called with parameters such that
+    no solution exists, it returns an empty list.
+    """
+
     T = M * N
     solver_base, vars_base = build_knight_tour(M, N, i0, j0, mode='sc')
     base_solutions, _ = extract_all_solutions(solver_base, M, N, T, vars_base)
